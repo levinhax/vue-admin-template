f7a2024a4cde494709c6f229825e3f14a9b211cf
levin <levin1229@163.com>
2020-06-15 11:25:40 +0800

    fix: 初始化数据获取

--- src/permission.js
+++ src/permission.js
@@ -39,40 +39,29 @@ router.afterEach((to, from) => {
   // NProgress.done() // 结束Progress
 })
 
-function getAsyncRouter(to, next) {
+async function getAsyncRouter(to, next) {
   const params = {
     userRole: store.getters.token
   }
-  getRouter(params)
-    .then(res => {
-      // console.log('---------- 获取动态路由 ---------')
-      // console.log(res.data.router)
-      // console.log('---------- 获取动态路由 ---------')
-      const asyncRouter = addRouter(res.data.router) // 进行递归解析
-      return asyncRouter
+  const resRouter = await getRouter(params)
+  if (resRouter.code === 200) {
+    const asyncRouter = addRouter(resRouter.data.router)
+    // 后置添加404页面,防止刷新404
+    asyncRouter.push({
+      path: '*',
+      redirect: '/404',
+      hidden: true
     })
-    .then(asyncRouter => {
-      // 后置添加404页面,防止刷新404
-      asyncRouter.push({
-        path: '*',
-        redirect: '/404',
-        hidden: true
-      })
-      router.addRoutes(asyncRouter) // vue-router提供的addRouter方法进行路由拼接
-      // console.log('---------- 解析后的动态路由 ---------')
-      // console.log(asyncRouter)
-      // console.log('---------- 解析后的动态路由 ---------')
-      store.dispatch('permission/generateRoutes', asyncRouter) // 存储到vuex
-      store.dispatch('user/getInfo')
-      store.commit('user/SET_INIT', true)
-
-      // hack method to ensure that addRoutes is complete
-      // set the replace: true, so the navigation will not leave a history record
-      // https://router.vuejs.org/zh-cn/advanced/navigation-guards.html
-      next({ ...to, replace: true })
-    })
-    .catch(e => {
-      console.log('async router error: ', e)
-      removeToken()
-    })
+    router.addRoutes(asyncRouter) // vue-router提供的addRouter方法进行路由拼接
+    await store.dispatch('permission/generateRoutes', asyncRouter) // 存储到vuex
+    await store.dispatch('user/getInfo')
+    store.commit('user/SET_INIT', true)
+    // hack method to ensure that addRoutes is complete
+    // set the replace: true, so the navigation will not leave a history record
+    // https://router.vuejs.org/zh-cn/advanced/navigation-guards.html
+    next({ ...to, replace: true })
+  } else {
+    console.log('============ async router error ===========')
+    removeToken()
+  }
 }
